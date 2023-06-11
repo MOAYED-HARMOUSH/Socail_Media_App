30d1131e43b2ea867ec76e977bb7a7db7ea7171e
YSRABD <sawkerakaijielite@gmail.com>
2023-06-11 13:04:27 +0300

    Log in Done

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,14 +6,14 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\AuthRequest;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function signUp(AuthRequest $request)
     {
-        $request->validated();
-        $user = User::create($request->all());
-        $token = $user->createToken('Sign up',[''],now()->addYear())->plainTextToken;
+        $user = User::create($request->validated());
+        $token = $user->createToken('Sign up', [''], now()->addYear())->plainTextToken;
         $user->specialty()->create($request->all());
         return response()->json([
             'token' => $token
@@ -22,7 +22,30 @@ class AuthController extends Controller
 
     public function logIn(Request $request)
     {
-        # code...
+        // $data = $request->validate([
+        //     'email' => 'bail|required|email',
+        //     'password' => 'bail|required|string|min:8'
+        // ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user) {
+            return response()->json([
+                'Message' => "this $request->email email doesn't Exist or Invalid , Please Make sure you Enter the right one or Sign up"
+            ]);
+        }
+
+        if (!Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'Message' => 'Password Incorrect Please Make sure you Enter the right one or Press Forget Password Button'
+            ]);
+        }
+
+        $token = $user->createToken('Log in', [''], now()->addYear())->plainTextToken;
+
+        return response()->json([
+            'token' => $token
+        ]);
     }
 
     public function hasLoggedIn(Request $request)
--- routes/Api/auth.php
+++ routes/Api/auth.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::post('signUp','signUp')->name('create');
 
 Route::get('first', 'hasLoggedIn')->middleware(['auth:sanctum']);
+
+Route::post('logIn', 'logIn');
