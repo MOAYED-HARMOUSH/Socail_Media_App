193f1a66db67e82cff9e54c472f6f2bce16d179b
YSRABD <sawkerakaijielite@gmail.com>
2023-07-06 17:38:00 +0300

    gg

--- database/migrations/2023_06_04_031750_create_invites_table.php
+++ database/migrations/2023_06_04_031750_create_invites_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('invites', function (Blueprint $table) {
             $table->id();
-            $table->boolean('is_approved')->nullable();
 
             $table->foreignId('sender')
                 ->constrained('users')
--- routes/Api/auth.resetpassword.php
+++ routes/Api/auth.resetpassword.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('forgotPassword', 'forgotPassword');
+Route::post('forgot', 'forgotPassword');
 
 Route::post('checkToken', 'checkToken');
 
-Route::post('resetPassword', 'resetPassword');
+Route::post('reset', 'resetPassword');
