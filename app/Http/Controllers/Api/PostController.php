be8e70960dadb9ae8cdd870a035f452292a102aa
MOAYED-HARMOUSH <moayad.harmoush@gmail.com>
2023-08-19 02:00:27 +0300

    d

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -229,8 +229,10 @@ class PostController extends Controller
         if ($first_fifteen == null) {
             $count = counterpost::where('user_id', $user->id)->where('location', 'homepage')->delete();
 
-            return 'end posts >> referssh';
-            //      //$l=0;
+            return response()->json([
+                'Message' => 'end posts >> referssh',
+                'data' => ['posts' =>[]]
+            ]);            //      //$l=0;
             //    return $this->gethomeposts($request );
         }
         return response()->json([
